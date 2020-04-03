10df36686069195e4362fe5b469fcc3dee75b271
Brian McMullen <brianmcmullenbiochem@gmail.com>
2020-04-03 21:37:26 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -10,7 +10,7 @@ describe('index', () => {
 
   it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
-
+    console.error("Hi")
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
